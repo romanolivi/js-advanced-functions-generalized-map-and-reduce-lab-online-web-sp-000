9d88c2d6971862740b4be7adbf9d11d3bafc02d1
romanolivi <romano_livi@outlook.com>
2021-03-25 19:32:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, fn) {
+    let a = []
+    for (const element of array) {
+        a.push(fn(element));
+    }
+    return a
+}
+
+function reduce(array, fn, start = 0) {
+    let subtotal = start
+    if (typeof fn(1, true) == "boolean"){
+        subtotal = true
+    }
+    for (const element of array) {
+        subtotal = fn(element, subtotal)
+    }
+    return subtotal
+}
